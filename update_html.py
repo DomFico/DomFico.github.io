6a036058a3cc32ec7cf2c53599406fec950c49df
Dominic Fico <daf0030@auburn.edu>
2024-08-02 09:17:51 -0500

    test

---
+++ update_html.py
@@ -0,0 +1,40 @@
+import os
+import json
+
+# Define the directories to be scanned
+directories = {
+    "Coding Projects": "Coding Projects",
+    "Research": "Research"
+}
+
+# Function to generate HTML for a list of files
+def generate_file_list_html(folder, files):
+    file_links = [f'<a href="{folder}/{file}">{file}</a>' for file in files]
+    return '\n'.join(f'<div>{link}</div>' for link in file_links)
+
+# Read the current index.html
+with open('index.html', 'r') as file:
+    lines = file.readlines()
+
+# Find the placeholders in the HTML where the file lists will be inserted
+coding_projects_start = lines.index('<!-- Coding Projects Start -->\n')
+coding_projects_end = lines.index('<!-- Coding Projects End -->\n')
+research_start = lines.index('<!-- Research Start -->\n')
+research_end = lines.index('<!-- Research End -->\n')
+
+# Generate the new HTML content for each directory
+coding_projects_files = os.listdir(directories['Coding Projects'])
+research_files = os.listdir(directories['Research'])
+
+coding_projects_html = generate_file_list_html(directories['Coding Projects'], coding_projects_files)
+research_html = generate_file_list_html(directories['Research'], research_files)
+
+# Update the HTML content
+lines[coding_projects_start + 1:coding_projects_end] = [coding_projects_html + '\n']
+lines[research_start + 1:research_end] = [research_html + '\n']
+
+# Write the updated HTML back to the file
+with open('index.html', 'w') as file:
+    file.writelines(lines)
+
+print("index.html has been updated.")
